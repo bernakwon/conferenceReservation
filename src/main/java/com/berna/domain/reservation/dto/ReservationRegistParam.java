d55b1a274a7f706465a0ef9c18294197daa0d240
hrkwon <hrkwon@etners.com>
2019-12-06 08:55:59 +0900

    테스트데이터 변경 및 생성자 어노테이션 추가

--- src/main/java/com/berna/domain/reservation/dto/ReservationRegistParam.java
+++ src/main/java/com/berna/domain/reservation/dto/ReservationRegistParam.java
@@ -4,30 +4,14 @@ import java.time.LocalDate;
 
 import java.time.LocalTime;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Value;
+import lombok.*;
 
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReservationRegistParam {
-	@Builder
-   public ReservationRegistParam(Long conferenceId, LocalDate reservationDate, String registedName, String startTime,
-			String endTime, int repetitionOfNum, String reservationName, Long reservationId) {
-		super();
-		this.conferenceId = conferenceId;
-		this.reservationDate = reservationDate;
-		this.registedName = registedName;
-		this.startTime = startTime;
-		this.endTime = endTime;
-		this.repetitionOfNum = repetitionOfNum;
-		this.reservationName = reservationName;
-		this.reservationId = reservationId;
-	}
-public ReservationRegistParam() {
-		// TODO Auto-generated constructor stub
-	}
+
 
 private Long conferenceId;
    private LocalDate reservationDate;
